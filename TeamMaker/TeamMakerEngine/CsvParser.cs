7b6dd0629fdac3dd9f9152f2644416d9628e4a19
Sean Cornwell <sean@revdata.com>
2016-07-06 22:32:05 -0700

    initial creation of csvparser.cs, player.cs, iparser.cs

---
+++ TeamMaker/TeamMakerEngine/CsvParser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMakerEngine
+{
+    class CsvParser : IParser
+    {
+        public List<Player> Parse(string filepath)
+        {
+            using (TextFieldParser parser = new TextFieldParser(""))
+            {
+                parser.SetDelimiters(new string[] { "," });
+                parser.HasFieldsEnclosedInQuotes = true;
+
+                // skip over header line.
+                parser.ReadLine();
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    //create new player and assign properties here
+                }
+            }
+
+            return null;
+        }
+    }
+}
