9c4ba2b98b5ea7ef1f14e9437f20bff45d0ac7c3
Danny Warren <dannydwarren@gmail.com>
2016-08-25 15:56:06 -0700

    Sample is now sanitizing first and last name columns

--- TeamMaker/SampleDataCleanser/Program.fs
+++ TeamMaker/SampleDataCleanser/Program.fs
@@ -6,30 +6,63 @@ open System.IO
 open Microsoft.VisualBasic.FileIO
 
 
+let firstNameIndex = 0
+let lastNameIndex = 1
+let schoolNameIndex = 9
+let buddy1Index = 13
+let buddy2Index = 14
+let headCoachIndex = 15
+
+
+let sanitizeNames ( dirtyStr : string ) ( names : string array ) =
+    let z = Array.get names 0
+
+    let a = dirtyStr.Replace (z, "")
+
+    ()
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
 
     let filePath = "D:\Desktop\SamplePlayersCsv.csv"
+    let filePathOut = "D:\Desktop\SamplePlayersCsv_Output.csv"
 
-    use parser = new TextFieldParser (filePath)
+    let parser = new TextFieldParser (filePath)
     parser.Delimiters <- [|","|]
     parser.HasFieldsEnclosedInQuotes <- true
-    parser.ReadLine |> ignore
 
-    let mutable info = []
+    let mutable names = []
+    let mutable schools = []
+    let mutable buddy1s = []
+    let mutable buddy2s = []
+    let mutable coaches = []
+
+    let mutable count = -2
+
+    parser.ReadFields |> ignore 
+    parser.ReadFields |> ignore
 
     while not parser.EndOfData do
         let playerFields = parser.ReadFields ()
-        info <- List.append info [ [playerFields.[0], playerFields.[1], playerFields.[9], playerFields.[13], playerFields.[14], playerFields.[15] ] ]
+        count <- count + 1
+        if count > 0 then do
+            names <- List.append names [ [| playerFields.[firstNameIndex] + "," + playerFields.[lastNameIndex]; playerFields.[firstNameIndex] + " " + playerFields.[lastNameIndex]; sprintf "First%d" count; sprintf "Last%d" count; sprintf "First%d,Last%d" count count |] ]
+            schools <- List.append schools [ [| playerFields.[schoolNameIndex] |] ]
+            buddy1s <- List.append buddy1s [ [| playerFields.[buddy1Index] |] ]
+            buddy2s <- List.append buddy2s [ [| playerFields.[buddy2Index] |] ]
+            coaches <- List.append coaches [ [| playerFields.[headCoachIndex] |] ]
 
-    //let a = File.ReadAllText filePath
-    let b =
-        info
-        |> List.map (fun x -> info |> List.map (fun y -> (x, y)))
-        |> List.concat
-        |> List.map (fun (x, y) -> x + y)
+    parser.Dispose |> ignore
 
-    File.WriteAllText (filePath, b)
+    let mutable toSanitize = File.ReadAllText filePath
+
+    for i = 0 to names.Length - 1 do
+        let o = names.[i].[0]
+        let n = names.[i].[4]
+        toSanitize <- toSanitize.Replace (o,n)
+        ()
+
+    File.WriteAllText (filePathOut, toSanitize)
 
     0 // return an integer exit code
