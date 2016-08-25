f3452902167e1009ee46bcaf7c718eefc556acb5
Sean Cornwell <sean@revdata.com>
2016-08-24 22:40:57 -0700

    Implemented csv parser

--- TeamMaker/TeamMakerEngine/CsvParser.cs
+++ TeamMaker/TeamMakerEngine/CsvParser.cs
@@ -11,6 +11,8 @@ namespace TeamMakerEngine
     {
         public List<Player> Parse(string filepath)
         {
+            List<Player> players = new List<Player>();
+
             using (TextFieldParser parser = new TextFieldParser(""))
             {
                 parser.SetDelimiters(new string[] { "," });
@@ -19,14 +21,56 @@ namespace TeamMakerEngine
                 // skip over header line.
                 parser.ReadLine();
 
+                int firstNameIndex = 0;
+                int lastNameIndex = 1;
+                int addressIndex = 2;
+                int cityIndex = 3;
+                int zipIndex = 4;
+                int stateIndex = 5;
+                int dobIndex = 6;
+                int ageGroupIndex = 7;
+                int allStarIndex = 8;
+                int schoolNameIndex = 9;
+                int gradeIndex = 10;
+                int medicalIndex = 11;
+                int specialRequestsIndex = 12;
+                int buddyRequest1Index = 13;
+                int buddyRequest2Index = 14;
+                int headCoachRequestIndex = 15;
+                int volunteersNeededIndex = 16;
+
                 while (!parser.EndOfData)
                 {
                     string[] fields = parser.ReadFields();
+
+                    if (fields == null)
+                        continue;
+
+                    var player = new Player(
+                        firstName: fields[firstNameIndex],
+                        lastName: fields[lastNameIndex],
+                        address: fields[addressIndex],
+                        city: fields[cityIndex],
+                        zip: fields[zipIndex],
+                        state: fields[stateIndex],
+                        dob: fields[dobIndex],
+                        ageGroup: fields[ageGroupIndex],
+                        allStar: fields[allStarIndex],
+                        schoolName: fields[schoolNameIndex],
+                        grade: fields[gradeIndex],
+                        medical: fields[medicalIndex],
+                        specialRequests: fields[specialRequestsIndex],
+                        buddyRequest1: fields[buddyRequest1Index],
+                        buddyRequest2: fields[buddyRequest2Index],
+                        headCoachRequest: fields[headCoachRequestIndex],
+                        volunteersNeeded: fields[volunteersNeededIndex]);
+
+                    players.Add(player);
                     //create new player and assign properties here
                 }
             }
 
-            return null;
+            return players;
         }
     }
 }
