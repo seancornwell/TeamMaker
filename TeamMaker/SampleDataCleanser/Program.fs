13923fcf7c35af03b8bf8bc87f42090587cad9b1
Danny Warren <dannydwarren@gmail.com>
2016-08-26 14:28:50 -0700

    Schools field is now sanitized

--- TeamMaker/SampleDataCleanser/Program.fs
+++ TeamMaker/SampleDataCleanser/Program.fs
@@ -2,6 +2,7 @@
 // See the 'F# Tutorial' project for more help.
 module Program
 
+open System
 open System.IO
 open Microsoft.VisualBasic.FileIO
 
@@ -48,19 +49,30 @@ let main argv =
         count <- count + 1
         if count > 0 then do
             names <- List.append names [ [| playerFields.[firstNameIndex] + "," + playerFields.[lastNameIndex]; playerFields.[firstNameIndex] + " " + playerFields.[lastNameIndex]; sprintf "First%d" count; sprintf "Last%d" count; sprintf "First%d,Last%d" count count |] ]
-            schools <- List.append schools [ [| playerFields.[schoolNameIndex] |] ]
+            schools <- List.append schools [ playerFields.[schoolNameIndex] ]
             buddy1s <- List.append buddy1s [ [| playerFields.[buddy1Index] |] ]
             buddy2s <- List.append buddy2s [ [| playerFields.[buddy2Index] |] ]
             coaches <- List.append coaches [ [| playerFields.[headCoachIndex] |] ]
 
     parser.Dispose |> ignore
 
+
+    let distinctSchools = 
+        List.distinct schools
+        |> List.filter (fun x -> not (String.IsNullOrEmpty x) )
+        |> List.sortByDescending (fun x -> x.Length )
+        |> List.mapi (fun i x -> [| x; sprintf "School %d" (i + 1) |])
+     
     let mutable toSanitize = File.ReadAllText filePath
 
     for i = 0 to names.Length - 1 do
-        let o = names.[i].[0]
-        let n = names.[i].[4]
-        toSanitize <- toSanitize.Replace (o,n)
+        toSanitize <- toSanitize.Replace (names.[i].[0], names.[i].[4])
+    for i = 0 to distinctSchools.Length - 1 do
+        toSanitize <- toSanitize.Replace (distinctSchools.[i].[0], distinctSchools.[i].[1])
+//        toSanitize <- toSanitize.Replace (buddy1s.[i].[0], names.[i].[4])
+//        toSanitize <- toSanitize.Replace (buddy2s.[i].[0], names.[i].[4])
+//        toSanitize <- toSanitize.Replace (coaches.[i].[0], names.[i].[4])
+
         ()
 
     File.WriteAllText (filePathOut, toSanitize)
